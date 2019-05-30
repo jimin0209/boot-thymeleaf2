fcfbed8ac89931c73099bfacb0fc920ae8eec0b4
jimin0209 <induk@naver.com>
2019-05-30 17:25:27 +0900

    회원가입 로그인 로그아웃 처리

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -1,9 +1,12 @@
 package idu.cs.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,76 +22,109 @@ import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
+// 애노테이션 : 컴파일러에게 설정 내용이나 상태를 알려주는 목적, 적용범위가 클래스 내부로 한정
 public class HomeController {
-	@Autowired UserRepository userRepo; // Dependency Injection
-	
-	@GetMapping("/test")
-	public String home(Model model) {
-		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
-		return "index";
-	}
-	@GetMapping("/")
-	public String loadWelcome(Model model) {
-		return "index";
-	}
-	@GetMapping("/login")
-	public String loginUser(Model model) {
-		return "login";
-	}
-	@GetMapping("/users")
-	public String getAllUser(Model model) {
-		model.addAttribute("users", userRepo.findAll());
-		return "userlist";
-	}
-	@GetMapping("/users/{id}")
-	public String getUserById(@PathVariable(value = "id") Long userId,  
-	Model model) throws ResourceNotFoundException {
-		//User user = userRepo.findById(userId).get();
-		User user = userRepo.findById(userId)
-				.orElseThrow(() -> 
-				new ResourceNotFoundException("not found " + userId ));
-		model.addAttribute("id", "" + userId);
-		model.addAttribute("name", user.getName());
-		model.addAttribute("company", user.getCompany());
-		return "user";
-	}
-	@GetMapping("/regform")
-	public String loadRegForm(Model model) {		
-		return "regform";
-	}	
-	@PostMapping("/users")
-	public String createUser(@Valid @RequestBody User user, Model model) {
-		userRepo.save(user);
-		model.addAttribute("users", userRepo.findAll());
-		return "redirect:/users";
-	}
-	@PutMapping("/users/{id}")
-	// @RequestMapping(value="/users/{id}" method=RequestMethod.UPDATE)
-	public ResponseEntity<User> updateUserById(@PathVariable(value = "id") Long userId,  
-			@Valid @RequestBody User userDetails, Model model) throws ResourceNotFoundException { 
-		// userDetails 폼을 통해 전송된 객체, user는 id로 jpa를 통해서 가져온 객체
-		User user = userRepo.findById(userId) //userDetails.getId()
-				.orElseThrow(() -> 
-				new ResourceNotFoundException("not found " + userId ));
-		user.setName(userDetails.getName());
-		user.setCompany(userDetails.getCompany());
-		User userUpdate = userRepo.save(user); // 객체 삭제 -> jpa : record 삭제로 적용
-		return ResponseEntity.ok(userUpdate);
-	}
-	@DeleteMapping("/users/{id}")
-	// @RequestMapping(value="/users/{id}" method=RequestMethod.DELETE)
-	public String deleteUserById(@PathVariable(value = "id") Long userId,  
-	Model model) throws ResourceNotFoundException {		
-		User user = userRepo.findById(userId)
-				.orElseThrow(() -> 
-				new ResourceNotFoundException("not found " + userId ));
-		userRepo.delete(user); // 객체 삭제 -> jpa : record 삭제로 적용
-		model.addAttribute("name", user.getName());
-		return "disjoin";
-	}
-	@GetMapping("/disjoin")
-	public String disjoinForm(Model model) {		
-		return "disjoin";
-	}	
+   @Autowired UserRepository userRepo; //Dependency Injection
+   
+   @GetMapping("/")
+   public String loadWelcome(Model model){
+      return "index";
+   }
+   
+   @GetMapping("/login-form")
+   public String loginForm(Model model){
+      return "login";
+   }   
+   @GetMapping("/users")
+   public String getAllUser(Model model) {
+      model.addAttribute("users", userRepo.findAll());
+      return "userlist";
+   }
+   @GetMapping("/users/byname") // byname?name=***, *** 값이 name 변수에 저장
+   public String getUsersByName(@Param(value = "name") String name, Model model) {
+      List<User> users = userRepo.findByName(name);
+      model.addAttribute("users", users);
+      
+      return "userlist";
+   }
+   @GetMapping("/users/nameasc") // byname?name=***, *** 값이 name 변수에 저장
+   public String getUsersByNameAsc(@Param(value = "name") String name, Model model) {
+      List<User> users = userRepo.findByNameOrderByIdAsc(name);
+      model.addAttribute("users", users);
+      
+      return "userlist";
+   }
+   @PostMapping("/login")
+   // 실제 로그인 처리, user : 입력한 내용에 대한 객체, sessionUser : Repository로부터 가져온 내용의 객체
+   public String userLogin(@Valid User user, HttpSession session){
+      System.out.println("login process : ");
+      User sessionUser = userRepo.findByUserId(user.getUserId());
+      if(sessionUser == null) {
+         System.out.println("id error : ");
+         return "redirect:/login-form";
+      }
+      if(!sessionUser.getUserPw().equals(user.getUserPw())) {
+         System.out.println("pw error : ");
+         return "redirect:/login-form";
+      }
+      session.setAttribute("user", sessionUser);
+      return "redirect:/";
+   }   
+   @GetMapping("/logout")
+   public String logoutUser(HttpSession session){
+	   //session.invalidate();
+	   session.removeAttribute("user");
+      return "redirect:/";
+   }
+   @GetMapping("/users/{id}")
+   public String getUserById(@PathVariable(value = "id") Long userId, 
+         Model model ) throws ResourceNotFoundException {
+      //User user = userRepo.findById(userId).get();
+            User user = userRepo.findById(userId)
+                  .orElseThrow(() -> 
+                  new ResourceNotFoundException("not found " + userId ));
+      model.addAttribute("user", user);
+      return "user";
+   }   
+   @GetMapping("/register-form")
+   public String regform(Model model) {      
+      return "register";
+   }
+   @PutMapping("/users/{id}")
+   //@RequestMaping(value=""/users/{id}" method=RequestMethod.UPDATE)
+   public String updateUserById(@PathVariable(value = "id") Long userId, 
+         @Valid User userDetails, Model model ) throws ResourceNotFoundException {
+         // userDatials 폼을 통해 전송된 객체, user는 id로 jpa를 통해서 가져온 객체
+      User user = userRepo.findById(userId)
+                  .orElseThrow(() -> 
+                  new ResourceNotFoundException("not found " + userId ));
+      user.setName(userDetails.getName());
+      user.setCompany(userDetails.getCompany());
+      
+      User userUpdate = userRepo.save(user);
+      // 해당되는 user 의 아이디를 가지고 객체를 삭제 -> jpa : record 삭제로 적용
+      // model.addAttribute("name", user.getName());
+      
+      return "redirect:/users";
+      // 업데이트가 성공하면 users 자원을  get 방식으로 접근 하되 model에 user 어트리뷰트를 전달하면서 리 다이렉션
+      //return ResponseEntity.ok(userUpdate);
+   }   
+   @PostMapping("/users")
+   public String createUser(@Valid User user, Model model) {
+      userRepo.save(user);
+      model.addAttribute("user", userRepo.findAll());
+      return "redirect:/users";
+   }   
+   @DeleteMapping("/users/{id}")
+   //@RequestMaping(value=""/users/{id}" method=RequestMethod.DELETE)
+   public String deleteUserById(@PathVariable(value = "id") Long userId, 
+         Model model ) throws ResourceNotFoundException {
+            User user = userRepo.findById(userId)
+                  .orElseThrow(() -> 
+                  new ResourceNotFoundException("not found " + userId ));
+      userRepo.delete(user);
+      // 해당되는 user 의 아이디를 가지고 객체를 삭제 -> jpa : record 삭제로 적용
+      model.addAttribute("name", user.getName());
+      return "disjoin";
+   }  
 }
